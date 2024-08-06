b27e4543591306ad5b240c0dfc6ff09ed68de0f4
SliveFive5 <116616099+SliveFive5@users.noreply.github.com>
2024-08-06 10:05:31 +1000

    Very well Matthew here you go

--- electionDay.js
+++ electionDay.js
@@ -1,43 +1,45 @@
 seatNamesLen = 151;
+//This value needed to be hoisted to work for all the functions
 document.addEventListener("DOMContentLoaded", async function () {
     laborCount = 0;
-    liberalCount = 0;
+    liberalCount = 0; //This initializes the starting seat count, which is then added to later on
     greensCount = 0;
     otherCount = 0;
     parties = {
-        labor: { leader: "Anthony Albanese", colour: "red" },
-        liberal: { hue: `216`, colour: "blue" },
+        labor: { colour: "red" },
+        liberal: { colour: "blue" },
         green: { colour: "lightgreen" },
         other: { colour: "purple" }
-        // also im storing the colour as a hue value to be used as HSL colour, so i can just change the opacity based on percentages
     };
-    let raw = localStorage.getItem("campaign-trail-game-state");
+    let raw = localStorage.getItem("campaign-trail-game-state"); //Here, the electorate data is extracted from localStorage, where it was placed earlier in the process, allowing for the changes that took place there to impact here.
     let gameState = JSON.parse(raw);
-    let marginalSeats = localStorage.getItem("marginal-seats");
-    arrMarginalSeats = marginalSeats.split(",");
+    let marginalSeats = localStorage.getItem("marginal-seats"); //We also need to get this data, because later on we are going to compare the two.
+    arrMarginalSeats = marginalSeats.split(","); //This string has to be split by comma to make it an array
 
     document.addEventListener("mouseover", (mouseover) => {
+        //This is essentially a copy of the code shown in game.js, simply showing the user what they got in each state.
         let id = mouseover.target.id;
         const seatIDElement = document.getElementById("seatName");
         const seatPollElement = document.getElementById("pollData");
-        let seatNames = Object.keys(gameState); // [grey, lingiari]
+        let seatNames = Object.keys(gameState);
         for (let i = 0; i < seatNames.length; i++) {
-            let seatName = seatNames[i]; // "grey"
+            let seatName = seatNames[i];
             let pollData = gameState[seatName].polling;
             if (id === gameState[seatName].id) {
-                // let seat = gameState[seatName];
                 seatIDElement.innerText = seatName;
-                // var listPollData = Object.entries(pollData);
-                seatPollElement.innerText = `labor: ${pollData.labor}%\nliberal: ${pollData.liberal}%\nGreen: ${pollData.green}%\nOther: ${pollData.other}%`;
+                seatPollElement.innerText = `labor: ${Math.round(pollData.labor)}%\nliberal: ${Math.round(pollData.liberal)}%\nGreen: ${Math.round(pollData.green)}%\nOther: ${Math.round(
+                    pollData.other
+                )}%`;
             }
         }
     });
-    timeoutNum = 1000;
+    timeoutNum = 1000; //This is equivalent to 1 second. This number will go up as the code iterates over and over again, creating the impression of results coming in.
     for (let i = 0; i < 151; i++) {
-        let seatName = arrMarginalSeats[i];
+        let seatName = arrMarginalSeats[i]; //Here, we are trying to find the highestparty in the list of arrMarginalSeats, similar to the code in game.js
         let id = gameState[seatName]?.id;
         let seatElement = document.getElementById(id);
         let polling = gameState[seatName].polling;
+        console.log(polling);
         let partyNames = Object.keys(polling);
         let highestParty = partyNames[0];
         for (let i = 1; i < partyNames.length; i++) {
@@ -46,7 +48,13 @@ document.addEventListener("DOMContentLoaded", async function () {
                 highestParty = partyName;
             }
         }
+        greenFlowedLab = 0;
+        greenFlowedLib = 0;
         if (highestParty !== "green") {
+            if (seatName === "Melbourne") {
+                console.log("Melbourne");
+            }
+            //This code is identical to the code in game.js, checking preference flows
             greenFlowedLabPref = polling.green * 0.82;
             greenFlowedLibPref = polling.green * 0.178;
             greenFlowedLab = polling.labor + greenFlowedLabPref;
@@ -66,8 +74,14 @@ document.addEventListener("DOMContentLoaded", async function () {
             otherFlowedLibPref = polling.other * 0.493;
             otherFlowedLab = greenFlowedLab + otherFlowedLabPref;
             otherFlowedLib = greenFlowedLib + otherFlowedLibPref;
+            if (seatName === "Melbourne") {
+                console.log(otherFlowedLab);
+            }
             if (otherFlowedLab > otherFlowedLib && otherFlowedLab > polling.green) {
                 highestParty = "labor";
+                if (seatName === "Melbourne") {
+                    console.log("Not greens");
+                }
             }
             if (otherFlowedLib > polling.green && otherFlowedLib > otherFlowedLab) {
                 highestParty = "liberal";
@@ -77,6 +91,7 @@ document.addEventListener("DOMContentLoaded", async function () {
             }
         }
         if (highestParty === "labor") {
+            //This is the code that counts each individual seat for each party
             laborCount += 1;
         }
         if (highestParty === "liberal") {
@@ -89,27 +104,30 @@ document.addEventListener("DOMContentLoaded", async function () {
             otherCount += 1;
         }
         setTimeout(() => {
-            seatElement.style = `fill: ${parties[highestParty].colour};`;
-        }, timeoutNum);
-        timeoutNum = timeoutNum + 500;
+            seatElement.style = `fill: ${parties[highestParty].colour};`; //Now that the highestParty is known, we can change the colour
+        }, timeoutNum); //The timeout begins with just 1 second
+        timeoutNum = timeoutNum + 500; //Each time it iterates, more time is added. Since setTimeout is asynchrous, this means that the impression is created that the seats are actually being called in order of marginalness
     }
 });
 
 function openResults() {
+    //This code adds a table telling you the tabulated results.
     document.getElementById("labTabCount").innerText = laborCount;
     document.getElementById("libTabCount").innerText = liberalCount;
     document.getElementById("grnTabCount").innerText = greensCount;
     document.getElementById("othTabCount").innerText = otherCount;
 
-    let dialogTextSpan = document.getElementById("resultDialogSpan");
+    let dialogTextSpan = document.getElementById("resultDialogSpan"); //This code fires when the Show Results button is clicked, and it opens a dialog that displays the textual ending and tabular results.
     let dialog = document.querySelector("dialog");
     dialog.open = "open";
-    if (laborCount >= 76) {
-        dialogTextSpan.innerText = `Congratulations! You've won the election with a grand total of ${laborCount} seats. With this majority, you can [BE MORE SPECIFIC WHEN YOU WRITE THE QUESTIONS]`;
+    if (laborCount >= "76") {
+        //These are the different endings a player can get, based on whether they have won enough seats.
+        dialogTextSpan.innerText = `Congratulations! You've won the election with a grand total of ${laborCount} seats. With this majority, you've retaken Parliament for the Labor Party, and can finally make progress on the climate, make the economy fairer, and bring the country forward socially. As long as no major crisis occurs in your term, it should be smooth sailing from here...`;
     }
-    if (laborCount > liberalCount && laborCount > greensCount && laborCount < 76) {
+    if (laborCount > liberalCount && laborCount > greensCount && laborCount < "76") {
         dialogTextSpan.innerText = `In a shocking result, the election has returned a hung parliament. Thankfully however, you are the biggest party, with ${laborCount} seats. This means you will have to form a government with the crossbench.`;
-    } else {
+    }
+    if (labor) {
         dialogTextSpan.innerText = `Unfortunately, you have lost this election, only winning ${laborCount} seats.`;
     }
 }
--- game.js
+++ game.js
@@ -2,7 +2,7 @@
 //Here, a number of hoisted variables are defined, necessary for the functions to work
 var quesData = null;
 let questionCount = 1;
-const NUMBER_OF_QUESTIONS = 3;
+const NUMBER_OF_QUESTIONS = 8;
 
 document.addEventListener("DOMContentLoaded", async function () {
     //All of the below actions will only take place once loaded
@@ -17,6 +17,7 @@ document.addEventListener("DOMContentLoaded", async function () {
     let res = await fetch("./electorate.json");
     let jsonData = await res.json();
     gameState = jsonData;
+    //Here, we lay out the initial state of polling direct from the electorate.json
     let ques = await fetch("./question.json");
     quesData = await ques.json();
     // These actions only take place when the JSON has loaded
@@ -29,13 +30,15 @@ document.addEventListener("DOMContentLoaded", async function () {
         const seatIDElement = document.getElementById("seatName");
         const seatPollElement = document.getElementById("pollData");
         const seatWeightElement = document.getElementById("weightData");
-        let seatNames = Object.keys(gameState); // [grey, lingiari]
+        //Each of the above corresponds to a different element which will be shown when a mouserover event is detected, the name of the seat, the polling information, and the seat weightings
+        let seatNames = Object.keys(gameState); // [grey, lingiari, etc.]
         for (let i = 0; i < seatNames.length; i++) {
-            let seatName = seatNames[i]; // "grey"
-            let pollData = gameState[seatName].polling;
-            let weightData = gameState[seatName].weighting;
+            // Goes through all of the seats in the gamestate
+            let seatName = seatNames[i]; // "grey", for example
+            let pollData = gameState[seatName].polling; //Gets the polling for the seat chosen
+            let weightData = gameState[seatName].weighting; //Gets the weighting for the seat chosen
             if (weightData.Economy <= 1 && weightData.Economy > 0) {
-                //This code gives unique labels to the weightings based on their
+                //This code gives unique labels to a seat based on it's weightings
                 ideoLabel = "Slightly Liberal";
             }
             if (weightData.Economy < 0 && weightData.Economy >= -1) {
@@ -108,87 +111,89 @@ document.addEventListener("DOMContentLoaded", async function () {
                 cliLabel = "Hoax";
             }
             if (id === gameState[seatName].id) {
-                //By checking if the ID of the hover matches any of the
-                // let seat = gameState[seatName];
-                seatIDElement.innerText = seatName;
-                // var listPollData = Object.entries(pollData);
-                seatPollElement.innerText = `labor: ${pollData.labor}%\nliberal: ${pollData.liberal}%\nGreen: ${pollData.green}%\nOther: ${pollData.other}%`;
-                seatWeightElement.innerText = `Economy: ${ideoLabel}\nSocial: ${socLabel}\nTaxes: ${taxLabel}\nClimate: ${cliLabel}`;
+                //This is essentially a search function, as it looks through the list of seat id's to see which one matches the id the user has hovered over
+                seatIDElement.innerText = seatName; //This is the code that adds the name to the InfoBox
+                seatPollElement.innerText = `labor: ${Math.round(pollData.labor)}%\nliberal: ${Math.round(pollData.liberal)}%\nGreen: ${Math.round(pollData.green)}%\nOther: ${Math.round(
+                    pollData.other
+                )}%`; //This is the code that adds the polling to the infoBox
+                seatWeightElement.innerText = `Economy: ${ideoLabel}\nSocial: ${socLabel}\nTaxes: ${taxLabel}\nClimate: ${cliLabel}`; //This is the code that adds the weighting to the infoBox
             }
         }
     });
 });
 
 function closeDialog() {
+    //This function is used to close the question dialog, revealing the map underneath
     let dialog = document.querySelector("dialog");
     dialog.open = null; // = "open"
 }
 
 function openDialog() {
+    //Opens the question dialog again, allowing the user to go back to the question page
     let dialog = document.querySelector("dialog");
     dialog.open = "open";
 }
 function openAnsDialog(buttonNumber) {
+    //This is the function that runs when a user selects one of the answer buttons
     let seatNames = Object.keys(gameState);
-    ideoEffect = quesData[questionCount].answers[buttonNumber].effect.Economy;
+    ideoEffect = quesData[questionCount].answers[buttonNumber].effect.Economy; //These functions find the weightings for each of the 4 categories for the answer they have selected
     socEffect = quesData[questionCount].answers[buttonNumber].effect.Social;
     affEffect = quesData[questionCount].answers[buttonNumber].effect.Taxes;
+    climEffect = quesData[questionCount].answers[buttonNumber].effect.Climate;
+    genEffect = quesData[questionCount].answers[buttonNumber].effect.general;
     for (let i = 0; i < seatNames.length; i++) {
-        let seatName = seatNames[i];
+        //Iterates through all seats
+        let seatName = seatNames[i]; //^^
         let weightData = gameState[seatName].weighting;
-        ideoPollEffect = ideoEffect * weightData.Economy;
+        ideoPollEffect = ideoEffect * weightData.Economy; //This code multiplies the weighting for a seat on an issue by the weighting for the answer that the user has selected
         socPollEffect = socEffect * weightData.Social;
         affPollEffect = affEffect * weightData.Taxes;
+        climPollEffect = climEffect * weightData.Climate;
+        genPollEffect = genEffect * weightData.general;
         let pollData = gameState[seatName].polling;
-        pollChange = ideoPollEffect + socPollEffect + affPollEffect;
-        pollData.labor = pollData.labor + pollChange;
-        pollData.liberal = pollData.liberal - pollChange;
+        pollChange = ideoPollEffect + socPollEffect + affPollEffect + climPollEffect + genPollEffect; //This creates a polling effect derived on all of the weightings from an answer
+        pollData.labor = pollData.labor + pollChange; //If the pollChange is a negative, then labor will lose support
+        pollData.liberal = pollData.liberal - pollChange * 0.5; //Since the polls can't exceed 100%, other parties need to lose support as labor gains it, and vice versa
+        pollData.green = pollData.green - pollChange * 0.25; //Most of the support loss/gain comes from/goes to the Liberals, but a fair bit goes to the other parties. This simulates the dominance of Labor and the Liberal Party in politics
+        pollData.other = pollData.other - pollChange * 0.25;
     }
-    let something = document.getElementById("textFeedback");
-    something.innerText = quesData[questionCount].answers[buttonNumber].feedback;
+    let textFeedback = document.getElementById("textFeedback");
+    textFeedback.innerText = quesData[questionCount].answers[buttonNumber].feedback; //Finds the respective question and answer feedback
     let dialog = document.getElementById("advisorFeedback");
-    dialog.open = "open";
+    dialog.open = "open"; //The above 4 lines open the respective advisor feedback dialog
     document.getElementById("answerButton1").disabled = true;
     document.getElementById("answerButton2").disabled = true;
     document.getElementById("answerButton3").disabled = true;
     document.getElementById("answerButton4").disabled = true;
-    document.getElementById("mapButton").disabled = true;
-
-    const te = new TextEncoder();
-    window.crypto.subtle.encrypt(
-        {
-            name: "AES-GCM",
-            iv
-        },
-        key,
-        te.encode(JSON.stringify(gameState))
-    );
-    localStorage.setItem("campaign-trail-game-state", JSON.stringify(gameState));
+    document.getElementById("mapButton").disabled = true; //These lines prevent the user from clicking anything else while the advisor feedback button is up. This prevents them from sequence breaking or clicking more than one answer
 }
 function closeAnsDialog() {
+    //This function runs when the user closes the advisor feedback window, and it places the user back in the map screen, except it is changed to include a click event, and the user cannot open the questions until they have visited
     questionCount = questionCount + 1;
     let seatNames = Object.keys(gameState);
     document.getElementById("answerButton1").disabled = null;
     document.getElementById("answerButton2").disabled = null;
-    document.getElementById("answerButton3").disabled = null;
+    document.getElementById("answerButton3").disabled = null; //Reopens all of the answers buttons for future use, although the user won't actually be able to see them at this point
     document.getElementById("answerButton4").disabled = null;
     document.getElementById("mapButton").disabled = null;
     let dialog = document.getElementById("advisorFeedback");
-    dialog.open = null;
+    dialog.open = null; //This closes the question dialog and shows the map
     let otherDialog = document.querySelector("dialog");
     otherDialog.open = null; // = "open"
-    document.getElementById("questionButton").hidden = true;
-    document.getElementById("visitHeader").hidden = false;
+    document.getElementById("questionButton").hidden = true; //This gets rid of the button taking you back to the question screen
+    document.getElementById("visitHeader").hidden = false; //This shows the header that instructs the user to visit a state
     document.addEventListener("click", (visitClick) => {
-        let id = visitClick.target.id;
+        let id = visitClick.target.id; //Very similar to the mouseOver code, this runs the following only when there has been a click on an applicable object
         for (let i = 0; i < seatNames.length; i++) {
             let seatName = seatNames[i]; // "grey"
             if (id === gameState[seatName].id) {
-                console.log(seatName);
+                //This is another example of a search algorithm, looking through the seatlist to find
                 let confirmDialog = document.getElementById("confirmation");
                 const confirmText = document.getElementById("confirmationText");
-                confirmDialog.open = "open";
+                confirmDialog.open = "open"; //The buttons within this confirmDialog allow the user to either select a different seat, or it allows them
                 confirmText.innerText = `Do you want to campaign in ${seatName}?`;
+                let polling = gameState[seatName].polling; //polling is then extracted from the seat
+                polling.labor = polling.labor += 0.3;
             }
         }
     });
@@ -196,35 +201,40 @@ function closeAnsDialog() {
 function nextRound() {
     document.getElementById("confirmation").open = null;
     if (questionCount <= NUMBER_OF_QUESTIONS) {
-        //Remember to change that to whatever the question count turns out to be
+        //This can change in hoisted section depending on the number of questions written
         let dialog = document.querySelector("dialog");
         dialog.open = "open";
-        document.getElementById("questionButton").hidden = false;
+        document.getElementById("questionButton").hidden = false; //Now, it goes back to the question screen
         document.getElementById("visitHeader").hidden = true;
         let confirmDialog = document.getElementById("confirmation");
-        confirmDialog.open = null;
+        confirmDialog.open = null; //Closes the confirm dialog
         updateDisplay();
+        //The display is updated here to ensure decisions are reflected throughout gameplay, rather than just at the end
     } else {
+        //This is the code that will run when there are no more questions left, meaning the game is over and only the election day remains
+        //It sends you to the other html file
         window.location = "electionDay.html";
     }
 }
 function closeConfDialog() {
     document.getElementById("confirmation").open = null;
+    //This closes the confirmation dialog in case the user has changed their mind or misclicked
 }
 function updateDisplay() {
     let keys = Object.keys(gameState);
     let list = keys.map((key) => {
         let seat = gameState[key];
         seat.name = key;
-        return seat;
+        return seat; //This code updates the map with the latest results
     });
 
     const compareFn = (a, b) => {
-        // pick which is more marginal
+        // This code picks which the least marginal to most marginal seats for use in the electionDay file
 
         if (a.polling.labor > a.polling.liberal && a.polling.labor > a.polling.other && a.polling.labor > a.polling.green) {
-            if (a.polling.labor > b.polling.labor) return -1;
-            else if (a.polling.labor < b.polling.labor) return 1;
+            //This checks if the party is the most popular
+            if (a.polling.labor > b.polling.labor) return -1; //This checks if the polling for a is higher than b. If so, a -1 is returned, meaning they swap
+            else if (a.polling.labor < b.polling.labor) return 1; //On the other hand, if b is bigger, they don't swap
         } else if (a.polling.liberal > a.polling.labor && a.polling.liberal > a.polling.other && a.polling.liberal > a.polling.green) {
             if (a.polling.liberal > b.polling.liberal) return -1;
             else if (a.polling.liberal < b.polling.liberal) return 1;
@@ -237,11 +247,12 @@ function updateDisplay() {
         }
     };
 
-    let sorted = list.sort(compareFn);
-    let names = sorted.map((seat) => seat.name);
-    localStorage.setItem("marginal-seats", names);
+    let sorted = list.sort(compareFn); //This is a modified quicksort function using the sort() function
+    let names = sorted.map((seat) => seat.name); //This turns it into an array of the names of the seats in order
+    localStorage.setItem("marginal-seats", names); //Once sorted, the code is stored in localStorage for use in the electionDay webpage.
+    localStorage.setItem("campaign-trail-game-state", JSON.stringify(gameState));
 
-    const jsTextQuestion = document.getElementById("textQuestion");
+    const jsTextQuestion = document.getElementById("textQuestion"); //This code updates the question and the answer based on the new questionCount
     jsTextQuestion.innerText = quesData[questionCount].question;
     const jsTextAnswer1 = document.getElementById("answerButton1");
     const jsTextAnswer2 = document.getElementById("answerButton2");
@@ -255,56 +266,52 @@ function updateDisplay() {
     let seatNames = Object.keys(gameState); // [grey, lingiari]
     for (let i = 0; i < seatNames.length; i++) {
         let seatName = seatNames[i]; // "grey"
-        let id = gameState[seatName]?.id;
-        let seatElement = document.getElementById(id);
-        let polling = gameState[seatName].polling;
-        let partyNames = Object.keys(polling);
-        let highestParty = partyNames[0];
+        let id = gameState[seatName]?.id; //It then finds the ID of the seat in question
+        let seatElement = document.getElementById(id); //It takes the ID, and uses it to find the svg element in question
+        let polling = gameState[seatName].polling; //polling is then extracted from the seat
+        let partyNames = Object.keys(parties); //This polling used to construct a list of all the seats in the seat
+        let highestParty = partyNames[0]; //This initilizes the highestParty variable
         for (let i = 1; i < partyNames.length; i++) {
+            //The code loops through the partynames and checks which is the highest
             let partyName = partyNames[i];
             if (polling[partyName] > polling[highestParty]) {
                 highestParty = partyName;
             }
         }
-        // console.log(parties[highestParty]);
+
         if (highestParty !== "green") {
-            if (seatName === "Kennedy") {
-                // console.log("Gone Through");
-            }
-            greenFlowedLabPref = polling.green * 0.82;
-            greenFlowedLibPref = polling.green * 0.178;
+            //This is the preference flow code, which simulates the preferential voting system that Australia employs.
+            greenFlowedLabPref = polling.green * 0.82; //This is based off the observed flow of preference votes from the greens to labor in 2019
+            greenFlowedLibPref = polling.green * 0.178; //What this code ensures is that, in seats where Labor and Greens collectively rank higher than the liberals, that labor wins most of the time as occurs in real life
             greenFlowedLab = polling.labor + greenFlowedLabPref;
             greenFlowedLib = polling.liberal + greenFlowedLibPref;
             if (greenFlowedLab > greenFlowedLib && greenFlowedLab > polling.other) {
-                highestParty = "labor";
+                highestParty = "labor"; //It checks the highestParty again in case the preference flows change the outcome
             }
             if (greenFlowedLib > greenFlowedLab && greenFlowedLib > polling.other) {
                 highestParty = "liberal";
             }
             if (polling.other > greenFlowedLib && polling.other > greenFlowedLab) {
                 highestParty = "other";
-                // console.log("first other");
             }
         }
         if (highestParty !== "other") {
+            //If greens and other aren't the highest, then other will now spill it's preference flows
             otherFlowedLabPref = polling.other * 0.507;
             otherFlowedLibPref = polling.other * 0.493;
             otherFlowedLab = greenFlowedLab + otherFlowedLabPref;
             otherFlowedLib = greenFlowedLib + otherFlowedLibPref;
             if (otherFlowedLab > otherFlowedLib && otherFlowedLab > polling.green) {
                 highestParty = "labor";
-                // console.log("second lab");
             }
             if (otherFlowedLib > polling.green && otherFlowedLib > otherFlowedLab) {
                 highestParty = "liberal";
-                // console.log("second lib");
             }
             if (polling.green > otherFlowedLab && polling.green > otherFlowedLib) {
                 highestParty = "green";
-                // console.log("second grn");
             }
         }
-        marginColor = `${polling[highestParty]}%`;
-        seatElement.style = `fill: ${parties[highestParty].colour}; filter: opacity(${marginColor})`;
+        marginColor = `${polling[highestParty] - 10}%`; //This means that the higher the polling, the brighter the colour will be
+        seatElement.style = `fill: ${parties[highestParty].colour}; filter: opacity(${marginColor})`; //This is the code that actually fills in the necessary colour for every seat
     }
 }
